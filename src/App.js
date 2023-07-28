0dfabb69f05ca4b2b330ef570e848e84c3807fe0
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-07-27 20:50:05 -0700

    rr-5 finished commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,56 @@
-import {useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
+import { Fragment, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Gallery from './components/Gallery';
+import SearchBar from './components/SearchBar';
+import AlbumView from './components/AlbumView';
+import ArtistView from './components/ArtistView';
+// import { DataContext } from './context/DataContext';
 
 function App() {
-	let [search, setSearch] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+	let [search, setSearch] = useState('');
+	let [message, setMessage] = useState('Search for Music!');
+	let [data, setData] = useState([]);
 
-	const API_URL = 'https://itunes.apple.com/search?term='
+	const API_URL = 'https://itunes.apple.com/search?term=';
 
 	useEffect(() => {
 		if(search) {
 			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
+				document.title = `${search} Music`;
+				const response = await fetch(API_URL + search);
+				const resData = await response.json();
 				if (resData.results.length > 0) {
-					setData(resData.results)
+					setData(resData.results);
 				} else {
-					setMessage('Not Found')
+					setMessage('Not Found');
 				}
 			}
-			fetchData()
+			fetchData();
 		}
-	}, [search])
+	}, [search]);
 	
 	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
+		e.preventDefault();
+		setSearch(term);
 	}
 
 	return (
 		<div>
-			<SearchBar handleSearch = {handleSearch}/>
 			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<SearchBar handleSearch = {handleSearch}/>
+							<Gallery data={data} />
+						</Fragment>
+					} />
+					<Route path='/album/:id' element={<AlbumView />} />
+					<Route path='/artist/:id' element={<ArtistView />} />
+				</Routes>
+			</Router>
 		</div>
-  	);
+  );
 }
 
 export default App;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,21 +1,20 @@
-// Gallery.js
-import { useContext } from 'react'
-import { DataContext } from '../context/DataContext'
-import GalleryItem from './GalleryItem'
+// import { useContext } from 'react';
+// import { DataContext } from '../context/DataContext';
+import GalleryItem from './GalleryItem';
 
-function Gallery(){
-    const data = useContext(DataContext)
+function Gallery(props) {
+    // const data = useContext(DataContext);
     
-    const display = data.map((item,index) => {
+    const display = props.data.map((item,index) => {
         return (
             <GalleryItem item={item} key={index} />
-        )
-    })
+        );
+    });
     return (
         <div>
             {display}
         </div>
-    )
+    );
 }
 
-export default Gallery
+export default Gallery;
